6a7ccd2c1df24391dd554cc517006900f64a45c4
Henrique Ritter <henrique-ritter@hotmail.com>
2020-06-17 18:25:26 -0300

    Tests until CSV Upload

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Not enough balance in account');
+    }
+
+    let existCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!existCategory) {
+      existCategory = categoryRepository.create({
+        title: category,
+      });
+      await categoryRepository.save(existCategory);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: existCategory,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
